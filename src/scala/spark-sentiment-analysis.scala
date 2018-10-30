48359dc3dd12ebbe2999a0dd9583b8183e739e22
vavassoriluca <vavassoriluca@yahoo.com>
2018-10-30 13:07:29 +0100

    Scala Sentiment Analysis with Stanford coreNLP

--- src/scala/spark-sentiment-analysis.scala
+++ src/scala/spark-sentiment-analysis.scala
@@ -9,7 +9,52 @@ import org.apache.spark.SparkContext
 import org.apache.spark.streaming._
 import org.apache.spark.streaming.kafka._
 import java.util.{Date, Properties}
+import scala.collection.convert.wrapAll._
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, ProducerConfig}
+
+import edu.stanford.nlp.ling.CoreAnnotations
+import edu.stanford.nlp.neural.rnn.RNNCoreAnnotations
+import edu.stanford.nlp.pipeline.{Annotation, StanfordCoreNLP}
+import edu.stanford.nlp.sentiment.SentimentCoreAnnotations
+
+object Sentiment extends Enumeration {
+    type Sentiment = Value
+    val POSITIVE, NEGATIVE, NEUTRAL = Value
+
+    def toSentiment(sentiment: Int): Sentiment = sentiment match {
+        case x if x == 0 || x == 1 => Sentiment.NEGATIVE
+        case 2 => Sentiment.NEUTRAL
+        case x if x == 3 || x == 4 => Sentiment.POSITIVE
+    }
+}
+
+object SentimentAnalyzer {
+
+    val props = new Properties()
+    props.setProperty("annotators", "tokenize, ssplit, parse, sentiment")
+    val pipeline: StanfordCoreNLP = new StanfordCoreNLP(props)
+
+    def mainSentiment(input: String): Sentiment.Sentiment = Option(input) match {
+        case Some(text) if !text.isEmpty => extractSentiment(text)
+        case _ => throw new IllegalArgumentException("input can't be null or empty")
+    }
+
+    private def extractSentiment(text: String): Sentiment.Sentiment = {
+        val (_, sentiment) = extractSentiments(text)
+          .maxBy { case (sentence, _) => sentence.length }
+        sentiment
+    }
+
+    def extractSentiments(text: String): List[(String, Sentiment.Sentiment)] = {
+        val annotation: Annotation = pipeline.process(text)
+        val sentences = annotation.get(classOf[CoreAnnotations.SentencesAnnotation])
+        sentences
+          .map(sentence => (sentence, sentence.get(classOf[SentimentCoreAnnotations.SentimentAnnotatedTree])))
+          .map { case (sentence, tree) => (sentence.toString,Sentiment.toSentiment(RNNCoreAnnotations.getPredictedClass(tree))) }
+          .toList
+    }
+
+}
 
 object KafkaSpark {
 
@@ -18,8 +63,10 @@ object KafkaSpark {
         // make a connection to Kafka and read (key, value) pairs from it
         val conf = new SparkConf().setAppName("twitter-sentiment-analysis").setMaster("local[3]")
         val sc = SparkContext.getOrCreate(conf)
+        sc.setLogLevel("WARN")
         val ssc = new StreamingContext(sc, Seconds(1))    
         ssc.checkpoint("./checkpoint")
+
 
         val kafkaConf = Map(
             "metadata.broker.list" -> "localhost:9092",
@@ -28,10 +75,9 @@ object KafkaSpark {
             "zookeeper.connection.timeout.ms" -> "1000")
 
         val topics = Set("twitter")
-
         val tweets = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaConf, topics)
 
-        tweets.print()       
+        tweets.map{tweet => SentimentAnalyzer.mainSentiment(tweet._2)}.print()
 
         ssc.start()
         ssc.awaitTermination()
