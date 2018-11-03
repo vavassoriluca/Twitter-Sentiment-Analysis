3c3a34ccdd79a112ff4ba618a190fa51c4ab3289
andreicap <andrei.capastru@gmail.com>
2018-11-03 15:09:56 +0200

    Add gemfile for easier packages control

---
+++ src/ruby/kafka-producer-standalone.rb
@@ -0,0 +1,35 @@
+require 'tweetstream'
+require 'ruby-kafka'
+
+TweetStream.configure do |config|
+  config.consumer_key       =  ENV['consumer_key']
+  config.consumer_secret    =  ENV['consumer_secret']
+  config.oauth_token        = ENV['access_token']
+  config.oauth_token_secret = ENV['access_token_secret']
+  config.auth_method        = :oauth
+end
+
+logger = Logger.new($stderr)
+brokers = ["localhost:9092"]
+
+topic = "twitter"
+
+kafka = Kafka.new(brokers, client_id: "twitter-producer", logger: logger)
+
+producer = kafka.producer
+
+trackers = ['Drake']
+
+begin
+  TweetStream::Client.new.track(trackers) do |status|
+    p "#{status.user.name}"
+    p "#{status.text}"
+    print "\n\n\n"
+    producer.produce(status.text, topic: topic)
+    producer.deliver_messages
+  end
+  
+ensure
+  producer.deliver_messages
+  producer.shutdown
+end
